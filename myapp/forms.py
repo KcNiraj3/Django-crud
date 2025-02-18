529c9d18dfcd3d938cf0df2ccaa2bfa5ad599183
Niraj <nikc@pvamu.edu>
2025-02-18 16:52:56 -0600

    crud codes

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,4 @@
-from models import Student
+from .models import Student
 from django import forms
 
 class StudentForms(forms.ModelForm):
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-from . import Student
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForms
 
 # Create your views here.
 
-def home(request):
+def home(request, id=None): # default arguments
+    if id:
+        student_update = get_object_or_404(Student, id=id) 
+        form = StudentForms(request.POST or None,instance=student_update) 
+    else:
+        form = StudentForms(request.POST or None)    
+
+    form = StudentForms()
+    if request.method == 'POST':
+        form = form
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
     students = Student.objects.all()
     context ={
-        'students' : students
+        'students' : students ,
+        'form' : form
     }
     
-    return render(request, home.html, context = context)
+    return render(request, "home.html", context = context)
