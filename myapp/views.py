3157c264cbbd832c691c8dd5b1a276698a034ddd
Niraj <kcniraj44@gmail.com>
2025-02-18 23:22:15 -0600

    html code added

--- myapp/views.py
+++ myapp/views.py
@@ -7,12 +7,17 @@ from .forms import StudentForms
 def home(request, id=None): # default arguments
     if id:
         student_update = get_object_or_404(Student, id=id) 
-        form = StudentForms(request.POST or None,instance=student_update) 
+        form = StudentForms(request.POST or None,instance=student_update) # takes info or empty info and supply to  form as post request
     else:
-        form = StudentForms(request.POST or None)    
+        form = StudentForms(request.POST or None)    # if no need to update. will take create form
 
-    form = StudentForms()
+  
     if request.method == 'POST':
+        if "delete" in request.POST:
+            student_to_delete = get_object_or_404(Student, id=request.POST["delete"])
+            student_to_delete.delete()
+            return redirect("home")
+        
         form = form
         if form.is_valid():
             form.save()
